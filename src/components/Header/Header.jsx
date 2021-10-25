4446e98dbba2290b1c2990f6ab85ae0b7d0cf8a8
Sharad Agarwal <obviouslysharad@gmail.com>
2021-10-26 01:28:19 +0530

    Created a Navbar

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div>
+            <div className="headerWrapper">
+                <div className="topleft">
+                    Amanad
+                </div>
+                <div className="topright">
+                    Right Element
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Header
